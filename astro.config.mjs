f35a201ce9b70f4336519d159b84a9f4f22329c0
unibox <unibox@duck.com>
2025-09-21 11:01:55 +0200

    change adapter from node to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
-
 import sitemap from '@astrojs/sitemap';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-
-  adapter: node({
-    mode: 'standalone',
-  }),
-
-  output: 'server',
   integrations: [sitemap()],
+  adapter: netlify(),
 });
